eb079fc3b622dc6e5ab9f755951ed94665ebcc54
aishwarya <aishwaryakalgudi@gmail.com>
2020-03-19 11:21:47 +0530

    minor changes

--- PA2.c
+++ PA2.c
@@ -11,7 +11,7 @@ int counter = 0;
 
 void* increment(void* arg)
 {
-    for(int i=0; i<10000000; i++)
+    for(int i=0; i<1000000; i++)
     {
         flag[0] = 1;
         turn = 1;
@@ -26,7 +26,7 @@ void* increment(void* arg)
 
 void* decrement(void* arg)
 {
-    for(int j=0; j<10000000; j++)
+    for(int j=0; j<1000000; j++)
     {
         flag[1] = 1;
         turn = 0;
